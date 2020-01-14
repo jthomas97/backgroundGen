cc255d4338dff0f2a1d0bfbac7c708d538c1e24f
Jude <judethomas97@yahoo.com>
2020-01-13 17:18:19 -0800

    Uploaded files

---
+++ script.js
@@ -0,0 +1,17 @@
+let color1 = document.querySelector(".color1"),
+    color2 = document.querySelector(".color2"),
+    css    = document.querySelector("h3"),
+    body   = document.getElementById("gradient");
+
+function setGradient() {
+  body.style.background = 
+  "linear-gradient(to right, " 
+  + color1.value + ", " 
+  + color2.value + ")";
+
+  css.textContent = body.style.background + ";"
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient); 
